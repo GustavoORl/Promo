ba7aa39161e7c40948497a77e55f17d22f905ec2
GustavoORl <gustavo.orl19@gmail.com>
2026-02-13 15:44:19 -0300

    atualização back

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ import shopeeRoutes from "./routes/shopee.route.js"
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
